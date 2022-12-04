8eb9a81a2dfff927e9cfba6139054bc790951d6c
ushiken3 <110982457+ushiken3@users.noreply.github.com>
2022-12-04 20:45:00 +0900

    Update elements.js

--- src/assets/elements.js
+++ src/assets/elements.js
@@ -2,10 +2,43 @@ App.elements = {
    app: document.getElementById("app"),
    header: {
       root: document.createElement("div"),
-      hidari: document.createElement("div"),
+      hidari: {
+         root: document.createElement("div"),
+         img: document.createElement("img"),
+      },
+      migi: {
+         root: document.createElement("div"),
+         img: document.createElement("img"),
+
+      },
 
    },
    main: {
+      root: document.createElement("div"),
+      bgImg: document.createElement("img"),
+      productsContainer: {
+         root :document.createElement("div"),
+         titele: document.createElement("h1"),
+         description : document.createElement("p"),
+         products: {
+            root: document.createElement("div"),
 
-   }
+         },
+
+      } ,
+
+   },
+   cart:{
+      root: document.createElement("div"),
+      products:{
+         root: document.createElement("div"),
+      },
+      confirm: document.createElement("button"),
+
+   },
+   footer: {
+      root : document.createElement("div"),
+      img: document.createElement("img"),
+
+   },
 }

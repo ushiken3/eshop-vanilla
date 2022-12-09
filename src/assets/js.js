a8193bec75b04ea44f17dce3d3e6774dabb6b2a3
ushiken3 <110982457+ushiken3@users.noreply.github.com>
2022-12-09 23:52:35 +0900

    Update js.js

--- src/assets/js.js
+++ src/assets/js.js
@@ -2,8 +2,13 @@ const App = {
     init(){
         console.log("App Started")
         App.controllers.createStaticElements()
+        App.controllers.createHeader()
+        App.controllers.createMain()
+        App.controllers.createCart()
+        App.controllers.createFooter()
         console.log("App Ended", App)
     },
+
 
 
 }

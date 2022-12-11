e6b777792ea6a8ada50f9a647f5ba85be866bb62
ushiken3 <110982457+ushiken3@users.noreply.github.com>
2022-12-12 01:12:55 +0900

    challenge

--- src/assets/js.js
+++ src/assets/js.js
@@ -2,10 +2,7 @@ const App = {
     init(){
         console.log("App Started")
         App.controllers.createStaticElements()
-        App.controllers.createHeader()
-        App.controllers.createMain()
-        App.controllers.createCart()
-        App.controllers.createFooter()
+       
         console.log("App Ended", App)
     },
 

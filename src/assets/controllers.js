e383a3389e99c2a07082396786be7a6305f38978
ushiken3 <110982457+ushiken3@users.noreply.github.com>
2022-12-27 21:22:02 +0900

    Update index.html (#3)
    * Update index.html
    
    * tocyuu
    
    * とりあえず課題完了？
    
    * Update index.html
    
    * とちゅう
    
    * tocyuu
    
    * Update controllers.js
    
    Co-authored-by: Yo <yoannes@gmail.com>

--- src/assets/controllers.js
+++ src/assets/controllers.js
@@ -1,5 +1,106 @@
 App.controllers = {
 
+    navigate(to) {
+        history.pushState({page: 1}, null,`?page=${to}`)
+        //`はstringと変数を合体させる時に使う。
+        App.controllers.renderer()
+
+    },
+    renderer(){
+        const searchParams = new URLSearchParams(window.location.search)
+        const currentPage = searchParams.get("page")
+
+        if (currentPage ==="cart") {
+            App.controllers.showCart()
+        } else {
+            App.controllers.showMain()
+        }
+        },
+        card(product){
+
+
+            const el = document.createElement("div")
+
+            el.style.border = "1px solid black"
+
+            //image//
+            const img = document.createElement("img")
+            img.src= product.Image
+            img.style.maxHeight="300px"
+            img.style.maxWidth="300px"
+            img.style.borderRadius="50%"
+            
+
+
+            //title//
+            const title = document.createElement("div")
+            title.innerHTML = product.name
+
+            //price//
+            const price = document.createElement("div")
+            price.innerHTML = product.price
+
+            //description//
+            const desc = document.createElement("div")
+            desc.innerHTML = product.description
+
+            //action button//
+            const btn = document.createElement("button")
+            btn.innerHTML = "Add to cart"
+
+            el.appendChild(img)
+            el.appendChild(title)
+            el.appendChild(price)
+            el.appendChild(desc)
+            el.appendChild(btn)
+
+
+
+            return el
+
+
+        },
+
+        showMain() {
+            const els = App.elements
+
+            els.cart.root.style.display ="none"
+
+            els.main.root.style.display ="block"
+
+            els.main.root.style.background ="gray"
+
+            const cardContainer = document.createElement("div")
+
+            cardContainer.style.display ="flex"
+
+            cardContainer.style.flexWrap ="wrap"
+
+                els.main.root.innerHTML=""
+                els.main.root.appendChild(cardContainer)
+                App.state.products.forEach((product) =>{
+                    const card = App.controllers.card(product)                    
+                    cardContainer.appendChild(card)
+                    })
+
+           
+            
+            
+
+
+           
+          
+    
+        },
+        showCart(){
+            const els = App.elements
+
+            els.main.root.style.display ="none"
+
+            els.cart.root.style.display ="block"
+
+        },
+
     createHeader() {
         const els = App.elements
 
@@ -20,7 +121,21 @@ App.controllers = {
 
         header.migi.root.style.border = "1px solid black"
         header.migi.root.innerHTML = "migi"
-       
+
+       //temporary button for router
+       const btn =document.createElement("button")
+       btn.innerHTML = "Go to /cart"
+       btn.onclick = () =>{
+        App.controllers.navigate("cart")
+       },
+       header.hidari.root.appendChild(btn)
+       const btn2 = document.createElement("button")
+       btn2.innerHTML = "Go to /main"
+       btn2.onclick = () => {
+        App.controllers.navigate("main")
+       },
+       header.hidari.root.appendChild(btn2)
+
         header.root.appendChild(header.hidari.root)
         header.root.appendChild(header.migi.root)
         els.app.appendChild(header.root)
@@ -42,6 +157,7 @@ App.controllers = {
     createCart() {
         const els = App.elements
 
+        els.cart.root.style.display ="none"
         els.cart.root.style.border ="1px solid black"
         els.cart.root.innerHTML = "cart"
         els.cart.root.style.flexGrow =1
@@ -72,7 +188,7 @@ App.controllers = {
         
         App.controllers.createHeader()
         App.controllers.createMain()
-        // App.controllers.createCart()
+        App.controllers.createCart()
         App.controllers.createFooter()
         console.log("static Elements Created")
 
